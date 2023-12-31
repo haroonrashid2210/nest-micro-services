d7ce395f8d010fdf158b0cfeed28763a512039f1
haroon.turing <haroon@turingtechnologies.org>
2023-12-31 17:29:32 +0500

    added pino logger

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,6 +1,20 @@
+import { Type } from 'class-transformer';
+import { IsDate, IsMongoId, IsString } from 'class-validator';
+
 export class CreateReservationDto {
+  @IsDate()
+  @Type(() => Date)
   startDate: Date;
+
+  @IsDate()
+  @Type(() => Date)
   endDate: Date;
+
+  @IsString()
+  @IsMongoId()
   placeId: string;
+
+  @IsString()
+  @IsMongoId()
   invoiceId: string;
 }
